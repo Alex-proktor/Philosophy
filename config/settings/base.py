4d1ce99afcc1aca422e7d838f724049954edcb95
Alex I <alex.tutmee@gmail.com>
2019-01-18 12:25:22 +0300

    add config DB

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://localhost/philosophy'),
+    # 'default': env.db('DATABASE_URL', default='postgres://localhost/philosophy'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
